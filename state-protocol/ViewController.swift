2b98911c0dcb19d55b23fdc6e5104fbaa318db9f
RITESH GUPTA <rit.nsit@gmail.com>
2018-03-20 23:33:17 +0530

    Initial Commit

---
+++ state-protocol/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  state-protocol
+//
+//  Created by Ritesh Gupta on 20/03/18.
+//  Copyright © 2018 Ritesh Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
